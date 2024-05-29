52bdbfe29929a6867f468f21c03f885e7f44520e
such-a-nooob <93153157+such-a-nooob@users.noreply.github.com>
2024-05-30 01:49:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,4 +82,6 @@ def get_cap():
     return jsonify({"caption": caption,"time": timetaken})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+    #app.run(debug=True)
